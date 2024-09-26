a8a07f64fc57060eee43348a6636381e03350c34
bblitzKrieg <canpolo824@gmail.com>
2024-09-26 11:16:27 +0300

    Update API base URLs to use HTTPS

--- aioblitzkrieg/api.py
+++ aioblitzkrieg/api.py
@@ -30,8 +30,8 @@ class AioBlitzkrieg(BaseClient):
 
         super().__init__()
 
-        self.base_url = 'http://api.blitzkrieg.space/public/v1'
-        self.upload_base_url = 'http://uploadapi.blitzkrieg.space'
+        self.base_url = 'https://api.blitzkrieg.space/public/v1'
+        self.upload_base_url = 'https://uploadapi.blitzkrieg.space'
         self.api_key = api_key
 
         self.headers = {
